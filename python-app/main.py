ea7e8c3b5721cbe2457569699382804ae9580226
Alexandr Gattunen <agattunen@upteam.com>
2022-03-25 16:36:24 +0300

    docker course

---
+++ python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Welcome to the calendar\n')
+
+year = int(input('Enter year: '))
+month = int(input('Enter month: '))
+
+print(calendar.month(year, month))
+
+print('Good Lack!')
